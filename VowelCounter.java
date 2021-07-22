8d340994aa4e6fdbbe579317c8daf5f3126dd031
hniangtha <86883796+hniangtha@users.noreply.github.com>
2021-07-22 18:56:18 -0400

    Add files via upload

---
+++ VowelCounter.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        int a =0;
+        int e =0;
+        int i =0;
+        int o =0;
+        int u =0;
+        int nonVowel =0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String user = scan.next();
+
+        for(int k=0; k<user.length();k++){
+            char ch = user.charAt(k);
+            if (ch == 'a'){
+                a++;
+            }
+            if (ch == 'e'){
+                e++;
+            }
+            if (ch == 'i'){
+                i++;
+            }
+            if (ch == 'o'){
+                o++;
+            }
+            if (ch == 'u') {
+                u++;
+            }
+            if (ch !='a'&& ch !='e'&& ch !='i'&& ch !='o'&& ch !='u'){
+                nonVowel++;
+            }
+        }
+        System.out.println("a: " + a);
+        System.out.println("e: " + e);
+        System.out.println("i: " + i);
+        System.out.println("o: " + o);
+        System.out.println("u: " + u);
+        System.out.println("non-vowel: " + nonVowel);
+    }
+}
